f2c64d338c7cbce443615afd708f96d22bf8fd2b
mrod <matthewdavidrodgers@gmail.com>
2021-09-27 19:10:06 -0700

    Fmt internal/editor

--- internal/editor/editor.go
+++ internal/editor/editor.go
@@ -1,78 +1,78 @@
 package editor
 
 import (
-        "io"
+	"io"
 
-        "encoding/json"
-        "os"
-        "os/exec"
+	"encoding/json"
+	"os"
+	"os/exec"
 
-        "github.com/matthewdavidrodgers/quest/internal/http"
+	"github.com/matthewdavidrodgers/quest/internal/http"
 )
 
 type EncodeableRequestDetails struct {
-        Method  string            `json:"method"`
-        Url     string            `json:"name"`
-        Headers map[string]string `json:"headers"`
-        Body    *json.RawMessage  `json:"body"`
+	Method  string            `json:"method"`
+	Url     string            `json:"name"`
+	Headers map[string]string `json:"headers"`
+	Body    *json.RawMessage  `json:"body"`
 }
 
 func OpenEditorAndParseFromSavedContent(req *http.RequestDetails) error {
-        requestFile, err := os.Create("quest_editor_buf.json")
-        requestFileName := requestFile.Name()
-        if err != nil {
-                return err
-        }
+	requestFile, err := os.Create("quest_editor_buf.json")
+	requestFileName := requestFile.Name()
+	if err != nil {
+		return err
+	}
 
-        var emptyBody json.RawMessage = []byte("{}")
-        encodeableData := EncodeableRequestDetails{
-                Method: req.Method,
-                Url: req.Url,
-                Headers: req.Headers,
-                Body: &emptyBody,
-        }
-        encodedReq, err := json.MarshalIndent(encodeableData, "", "  ")
-        if err != nil {
-                return err
-        }
+	var emptyBody json.RawMessage = []byte("{}")
+	encodeableData := EncodeableRequestDetails{
+		Method:  req.Method,
+		Url:     req.Url,
+		Headers: req.Headers,
+		Body:    &emptyBody,
+	}
+	encodedReq, err := json.MarshalIndent(encodeableData, "", "  ")
+	if err != nil {
+		return err
+	}
 
-        _, err = requestFile.Write(encodedReq)
-        if err != nil {
-                return err
-        }
-        requestFile.Close()
+	_, err = requestFile.Write(encodedReq)
+	if err != nil {
+		return err
+	}
+	requestFile.Close()
 
-        editorProcess := exec.Command("vim", requestFileName)
+	editorProcess := exec.Command("vim", requestFileName)
 
-        editorProcess.Stdout = os.Stdout
-        editorProcess.Stdin = os.Stdin
-        editorProcess.Stderr = os.Stderr
+	editorProcess.Stdout = os.Stdout
+	editorProcess.Stdin = os.Stdin
+	editorProcess.Stderr = os.Stderr
 
-        editorProcess.Run()
-        if err != nil {
-                return err
-        }
+	editorProcess.Run()
+	if err != nil {
+		return err
+	}
 
-        requestFile, err = os.Open(requestFileName)
-        defer requestFile.Close()
-        defer os.Remove(requestFileName)
-        if err != nil {
-                return err
-        }
+	requestFile, err = os.Open(requestFileName)
+	defer requestFile.Close()
+	defer os.Remove(requestFileName)
+	if err != nil {
+		return err
+	}
 
-        contents, err := io.ReadAll(requestFile)
-        if err != nil {
-                return err
-        }
+	contents, err := io.ReadAll(requestFile)
+	if err != nil {
+		return err
+	}
 
-        err = json.Unmarshal(contents, &encodeableData)
-        if err != nil {
-                return err
-        }
-        req.Method = encodeableData.Method
-        req.Url = encodeableData.Url
-        req.Headers = encodeableData.Headers
-        req.Body = string(*encodeableData.Body)
+	err = json.Unmarshal(contents, &encodeableData)
+	if err != nil {
+		return err
+	}
+	req.Method = encodeableData.Method
+	req.Url = encodeableData.Url
+	req.Headers = encodeableData.Headers
+	req.Body = string(*encodeableData.Body)
 
-        return nil
+	return nil
 }
